63e230975025e42684c9ec02b3daae48a995d381
VasiliyRomanov <31405533+VasiliyRomanov@users.noreply.github.com>
2017-09-24 22:49:34 +0300

    Update and rename Task1v2.js to Task1v3.js

---
+++ Task1v3.js
@@ -0,0 +1,6 @@
+var employees = [
+	{name: 'Иван Пупкин', dep: 'Администрация', salary: '', ndfl: ''},
+	{name: 'Петр Гаврилкин', dep: 'IT-отдел', salary: '', ndfl: ''},
+	{name: 'Анна Смирнова', dep: 'Бухгалтерия', salary: '', ndfl: ''},
+	{name: 'Сергей Чашкин', dep: 'Снабжение', salary: '', ndfl: ''}
+];
